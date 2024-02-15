be51a77c0423820dec0fa4bf6eb4260720b090c0
vivan raj <vivanrajmittakodi6@gmail.com>
2024-02-15 17:23:00 +0530

    mongodb_client

--- server.js
+++ server.js
@@ -2,32 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const DataBase = require("./config/database");
-const routes = require('./Routes/route'); // Import the routes
+const routes = require('./Routes/route'); 
 
-// Make sure to use async keyword for the callback function
 DataBase();
 
 app.use(express.json());
 
-// Mount the routes
 app.use('/', routes);
 
 // Error handling middleware for unhandled routes
-// app.use((req, res, next) => {
-//   const error = new Error('Not Found');
-//   error.status = 404;
-//   next(error);
-// });
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
 
-// // Error handling middleware for server errors
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.json({
-//     error: {
-//       message: err.message,
-//     },
-//   });
-// });
+// Error handling middleware for server errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 app.listen(port, () => {
   console.log(`🚀 server running on PORT: ${port}`);
