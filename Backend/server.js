6ba97f64fc2574e1211b3e49f21e598cf2801ed8
vivan raj <vivanrajmittakodi6@gmail.com>
2024-02-20 17:33:35 +0530

    updated

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const DataBase = require("./config/database");
+const routes = require('./Routes/route'); 
+
+DataBase();
+
+app.use(express.json());
+
+app.use('/', routes);
+
+// Error handling middleware for unhandled routes
+// app.use((req, res, next) => {
+//   const error = new Error('Not Found');
+//   error.status = 404;
+//   next(error);
+// });
+
+// // Error handling middleware for server errors
+// app.use((err, req, res, next) => {
+//   const status = err.status || 500;
+//   res.status(status).json({
+//     error: {
+//       message: err.message || 'Internal Server Error',
+//     },
+//   });
+// });
+
+
+app.listen(port, () => {
+  console.log(`🚀 server running on PORT: ${port}`);
+});
---
+++ Frontend/src/Components/mainpage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './mainpage.css'; 
+
+function Mainpage() {
+  return (
+      <>
+    <div className='side_nav_container'>hyyy</div>
+      <div className='main_container'>
+        <div className='nav_container'>
+            <div>Home</div>
+            <div>You</div>
+            <div>Add New</div>
+        </div>
+        <div className='main_content_container'>bye</div>
+      </div>
+    </>
+  );
+}
+
+export default Mainpage;
